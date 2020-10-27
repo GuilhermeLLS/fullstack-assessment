45f1e922de2b84e7bfa2e78033ba0f9c0aea7dce
GuilhermeLLS <guiluizls@gmail.com>
2020-10-27 00:11:05 -0300

    chore(retreivers): add optional chaining

--- api/src/retrievers/index.ts
+++ api/src/retrievers/index.ts
@@ -2,9 +2,9 @@ import Participants, { ParticipantDocument } from '@models/Participant'
 
 export async function retrieveTotalParticipation(): Promise<number> {
     const [totalParticipationPercentage] = await Participants.aggregate([{ $group: { _id: null, total: { $sum: '$participation' } } }])
-    return totalParticipationPercentage.total
+    return totalParticipationPercentage?.total || 0
 }
 
-export async function retriveParticipants(): Promise<Array<ParticipantDocument>> {
+export async function retrieveParticipants(): Promise<Array<ParticipantDocument>> {
     return await Participants.find({}) as Array<ParticipantDocument>
 }
