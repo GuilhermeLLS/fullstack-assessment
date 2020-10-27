b29f826610bac41469d214f5f121b08d7ab36a9a
GuilhermeLLS <guiluizls@gmail.com>
2020-10-27 00:13:22 -0300

    feat(form): add new jss styles

--- front/src/components/form/index.tsx
+++ front/src/components/form/index.tsx
@@ -5,7 +5,37 @@ import { ParticipantsContext } from '../../context'
 
 const styles = {
     root: {
+        width: "100%",
+        padding: "10px 0",
+        backgroundColor: "#DADADA",
 
+        "& form": {
+            "@media(max-width: 768px)": {
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center"
+            }    
+        }
+
+    },
+    input: {
+        height: "30px",
+        margin: "0 15px 0 0",
+        border: "2px solid white",
+        "&::placeholder": {
+            padding: "0 0 0 5px",
+        },
+        "@media(max-width: 768px)": {
+            margin: "10px 0",
+            width: "250px",
+        }
+    },
+    button: {
+        padding: "10px",
+        border: "1px solid white",
+        width: "100px",
+        backgroundColor: "#DADADA",
+        color: "white"
     }
 }
 
@@ -19,36 +49,37 @@ interface FormData {
 }
 
 const Form: React.FC<FormProps> = (props: FormProps) => {
+    const { classes } = props
     const [formData, setFormData] = useState({})
     const [, setData] = useContext(ParticipantsContext)
-    
+
     const onHandleSubmit = (event: any) => {
         event.preventDefault()
         alert(JSON.stringify(formData))
         Axios.post("http://localhost:4000/participant", formData).then(res => setData(res.data))
+        event.target.reset()
         setFormData({
             name: "",
             lastname: "",
-            participantion: "",
+            participation: "",
         })
     }
 
     const changeHandler = (event: any) => {
         const nam = event.target.name;
         const val = event.target.value;
-        setFormData({...formData, [nam]: val});
+        setFormData({ ...formData, [nam]: val });
     }
-    
+
     return (
-        <form onSubmit={onHandleSubmit}>
-            <label>Name: </label>
-            <input type="text" name={"name"} onChange={changeHandler}/>
-            <label>Lastname: </label>
-            <input type="text" name={"lastname"} onChange={changeHandler}/>
-            <label>Participantion: </label>
-            <input type="text" name={"participation"} onChange={changeHandler}/>
-            <input type="submit"></input>
-        </form>
+        <div className={classes.root}>
+            <form onSubmit={onHandleSubmit}>
+                <input className={classes.input} placeholder={"Name"} type="text" name={"name"} onChange={changeHandler} />
+                <input className={classes.input} placeholder={"Last Name"} type="text" name={"lastname"} onChange={changeHandler} />
+                <input className={classes.input} placeholder={"Participation"} type="text" name={"participation"} onChange={changeHandler} />
+                <input className={classes.button} value={"Send"} type="submit" />
+            </form>
+        </div>
     );
 }
 
