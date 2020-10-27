47431bba9e461db87b852ede0e527dc66ee8c8c2
GuilhermeLLS <guiluizls@gmail.com>
2020-10-27 00:10:34 -0300

    feat(controller): add new delete controller

--- api/src/controllers/ParticipantsController.ts
+++ api/src/controllers/ParticipantsController.ts
@@ -1,5 +1,5 @@
 import Participants from '@models/Participant'
-import { retrieveTotalParticipation, retriveParticipants } from '@retrievers/index'
+import { retrieveTotalParticipation, retrieveParticipants } from '@retrievers/index'
 import { Request, Response } from 'express'
 
 class ParticipantsController {
@@ -9,20 +9,26 @@ class ParticipantsController {
       return res.status(404).send('A porcentagem total de participacao já é igual a 100. Nao e possivel adicionar mais participantes')
     }
 
-    const newParticipation = req.body.participation
+    const newParticipation = Number(req.body.participation)
+    console.log(totalParticipationPercentage, newParticipation, totalParticipationPercentage + newParticipation > 100);
     if (totalParticipationPercentage + newParticipation > 100) {
       const maxParticipationLeft = 100 - totalParticipationPercentage
       return res.status(404).send(`A porcentagem total de participacao ficara maior que 100. Voce deve inserir um valor de participacao menor ou igual a ${maxParticipationLeft}`)
     }
     
     await Participants.create(req.body)
-    const data = await retriveParticipants()
+    const data = await retrieveParticipants()
     return res.json(data)
   }
 
   async getParticipants(_: Request, res: Response) {
-    const data = await retriveParticipants()
+    const data = await retrieveParticipants()
     return res.json(data)
+  }
+
+  async deleteParticipants(_: Request, res: Response) {
+    await Participants.remove({})
+    return res.json([])
   }
 }
 
