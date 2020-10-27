28a7adb936548744d3d803eb21d2ff14266d3743
Guilherme Luiz <guiluizls@gmail.com>
2020-10-27 15:00:53 -0300

    feat(utils): add areObjectsEqual function

--- front/src/utils/index.ts
+++ front/src/utils/index.ts
@@ -1,4 +1,8 @@
-export function generateHexColor(arr: string[]): string {
+export function generateHexColor(existentColors: string[]): string {
     const color = `#${Math.floor(Math.random()*16777215).toString(16)}`
-    return arr.includes(color) ? generateHexColor(arr) : color
+    return existentColors.includes(color) ? generateHexColor(existentColors) : color
+}
+
+export function areObjectsEqual(value1: Record<string, any>, value2: Record<string, any>): boolean {
+    return JSON.stringify(value1) === JSON.stringify(value2)
 }
