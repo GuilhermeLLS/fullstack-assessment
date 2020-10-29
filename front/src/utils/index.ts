895bb7e59f08475d5b515723e7f19dc1512f7814
GuilhermeLLS <guiluizls@gmail.com>
2020-10-29 01:36:52 -0300

    refactor(utils): refactored generateHexColor

--- front/src/utils/index.ts
+++ front/src/utils/index.ts
@@ -1,5 +1,7 @@
 export function generateHexColor(existentColors: string[]): string {
-  const color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const parseHexString = (hexString: string) => hexString.length === 6 ? `${hexString}a` : hexString;
+  const color = parseHexString(`#${Math.ceil(Math.random() * 16777215).toString(16)}`);
+
   return existentColors.includes(color)
     ? generateHexColor(existentColors)
     : color;
