1555338370fd264163e8b44faa41c6f939e41421
GuilhermeLLS <guiluizls@gmail.com>
2020-10-29 00:17:51 -0300

    chore(babel): remove alias modeule plugin

--- api/babel.config.js
+++ api/babel.config.js
@@ -11,16 +11,8 @@ module.exports = {
     '@babel/preset-typescript'
   ],
   plugins: [
-    ['module-resolver', {
-      alias: {
-        '@config': './src/config',
-        '@models': './src/models',
-        '@controllers': './src/controllers',
-        '@views': './src/views'
-      }
-    }]
   ],
   ignore: [
-    '**/*.spec.ts'
+    '**/*.test.ts'
   ]
 }
