e47e50476e141750687612d80ba86e08abdd6708
GuilhermeLLS <guiluizls@gmail.com>
2020-10-29 23:32:19 -0300

    refactor(form): chnage props and styles

--- front/src/components/form/index.tsx
+++ front/src/components/form/index.tsx
@@ -1,7 +1,7 @@
 import Axios from "axios";
 import React, { useContext, useState } from "react";
 import withStyles, { WithStylesProps } from "react-jss";
-import { ParticipantsContext } from "../../context";
+import { ParticipantsContext } from "../context/context";
 import { ParticipantData } from "../../types";
 
 const styles = {
@@ -21,7 +21,7 @@ const styles = {
   input: {
     height: "30px",
     margin: "0 15px 0 0",
-    border: "2px solid white",
+    border: "2px solid #FFFFFF",
     "&::placeholder": {
       padding: "0 0 0 5px",
     },
@@ -32,26 +32,26 @@ const styles = {
   },
   button: {
     padding: "10px",
-    border: "1px solid white",
+    border: "1px solid #FFFFFF",
     width: "100px",
     backgroundColor: "#DADADA",
-    color: "white",
+    color: "#FFFFFF",
   },
 };
 
-interface FormProps extends WithStylesProps<typeof styles> {}
+interface FormProps extends WithStylesProps<typeof styles> { }
 
-const Form: React.FC<FormProps> = (props: FormProps) => {
-  const { classes } = props;
+const Form: React.FC<FormProps> = ({ classes }: FormProps) => {
   const [formData, setFormData] = useState({});
   const { setData } = useContext(ParticipantsContext);
 
-  const onHandleSubmit = (event: any): void => {
+  const onHandleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
     Axios.post<Array<ParticipantData>>("http://localhost:4000/participant", formData)
       .then((res) => setData(res.data))
       .catch((err) => alert(err.response.data));
-    event.target.reset();
+    const eventTarget = event.target as HTMLFormElement;
+    eventTarget.reset();
     setFormData({
       name: "",
       lastname: "",
@@ -60,9 +60,9 @@ const Form: React.FC<FormProps> = (props: FormProps) => {
   };
 
   const changeHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    const nam = event.target.name;
-    const val = event.target.value;
-    setFormData({ ...formData, [nam]: val });
+    const inputName = event.target.name;
+    const inputValue = event.target.value;
+    setFormData({ ...formData, [inputName]: inputValue });
   };
 
   return (
