55145851ff37e8698a6e75c60c023b9a4b1411cc
GuilhermeLLS <guiluizls@gmail.com>
2020-10-29 23:32:00 -0300

    test(form): update imports

--- front/src/components/form/index.test.tsx
+++ front/src/components/form/index.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Form from "./index";
 import { ParticipantData } from "../../types";
